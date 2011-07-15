727a6cd01f04589898feb46ea499ee25f458b1a0
Chehai Wu <chehai.wu@appfolio.com>
2011-07-14 21:27:28 -0700

    lookup logic

---
+++ src/remote_object.cpp
@@ -0,0 +1,12 @@
+#include "remote_object.h"
+
+RemoteObject::RemoteObject(std::string& path, Timestamp& t)
+:BackupObject(path, t)
+{
+	
+}
+
+RemoteObject::RemoteObject()
+{
+	
+}
---
+++ test/remote_object_test.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE remote_object_test
+#include <boost/test/unit_test.hpp>
+#include "../src/remote_object.h"
+
+BOOST_AUTO_TEST_CASE(constructor_test) 
+{
+	std::string ts("2002-01-20 23:59:59.000");
+	Timestamp t(boost::posix_time::time_from_string(ts));
+	std::string path = "abc";
+	RemoteObject ro0(path, t);
+	BOOST_CHECK_EQUAL(ro0.path(), path);
+	BOOST_CHECK_EQUAL(ro0.updated_at(), t);
+	BOOST_CHECK_EQUAL(ro0.status(), BackupObject::Valid);
+	
+	RemoteObject ro1;
+	BOOST_CHECK_EQUAL(ro1.status(), BackupObject::Valid);		
+}
